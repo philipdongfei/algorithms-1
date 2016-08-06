b912ec49d2524aef0c2158431ab97849208d2724
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-08-06 17:10:08 +0200

    Exercise Ex 1.1.5

---
+++ src/ch_1_1/Ex_1_1_5.java
@@ -0,0 +1,23 @@
+package ch_1_1;
+
+/**
+ * 1.1.5 Write a code fragment that prints true if the double variables x and y are both
+ * strictly between 0 and 1 and false otherwise.
+ */
+public class Ex_1_1_5 {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.exit(0);
+        }
+        checkDouble(Double.valueOf(args[0]), Double.valueOf(args[1]));
+    }
+
+    public static void checkDouble(double x, double y) {
+        if (x > 0 && x < 1 && y > 0 && y < 1) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
