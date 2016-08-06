f88187449848e9ca56d6ac5d0323af75307bfd1c
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-08-06 17:09:59 +0200

    Exercise Ex 1.1.4

---
+++ src/ch_1_1/Ex_1_1_4.java
@@ -0,0 +1,31 @@
+package ch_1_1;
+
+/**
+ * 1.1.4 What (if anything) is wrong with each of the following statements?
+ * a.
+ * b.
+ * c.
+ * d.
+ * if (a > b) then c = 0;
+ * if a > b { c = 0; }
+ * if (a > b) c = 0;
+ * if (a > b) c = 0 else b = 0;
+ */
+public class Ex_1_1_4 {
+
+    public static void main(String[] args) {
+        int a = 0, b = 0, c = 0;
+//      keyword then does not exist 
+//        if (a > b) then c = 0;
+
+//        lack of braces around a > b
+//        if a > b { c = 0; }
+
+//        looks good (and works)
+//        if (a > b) c = 0;
+
+//        lack of ; after c = 0
+//        if (a > b) c = 0 else b = 0;
+    }
+
+}
