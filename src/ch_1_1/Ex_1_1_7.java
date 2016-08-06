ff64b516fef9ab6ed168ff6509ed08de54c982df
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-08-06 17:10:32 +0200

    Exercise Ex 1.1.7

---
+++ src/ch_1_1/Ex_1_1_7.java
@@ -0,0 +1,49 @@
+package ch_1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * 1.1.7 Give the value printed by each of the following code fragments:
+ * a.
+ * double t = 9.0;
+ * while (Math.abs(t - 9.0/t) > .001)
+ * t = (9.0/t + t) / 2.0;
+ * StdOut.printf("%.5f\n", t);
+ * b.
+ * int sum = 0;
+ * for (int i = 1; i < 1000; i++)
+ * for (int j = 0; j < i; j++)
+ * sum++;
+ * StdOut.println(sum);
+ * c.
+ * int sum = 0;
+ * for (int i = 1; i < 1000; i *= 2)
+ * for (int j = 0; j < N; j++)
+ * sum++;
+ * StdOut.println(sum);
+ */
+public class Ex_1_1_7 {
+
+    public static void main(String[] args) {
+        // a.
+        double t = 9.0;
+        while (Math.abs(t - 9.0 / t) > .001)
+            t = (9.0 / t + t) / 2.0;
+        StdOut.printf("%.5f\n", t);
+
+        // b.
+//        int sum = 0;
+//        for (int i = 1; i < 1000; i++)
+//            for (int j = 0; j < i; j++)
+//                sum++;
+//        StdOut.println(sum);
+
+        // c.
+//        int sum = 0;
+//        for (int i = 1; i < 1000; i *= 2)
+//            for (int j = 0; j < N; j++)
+//                sum++;
+//        StdOut.println(sum);
+    }
+
+}
