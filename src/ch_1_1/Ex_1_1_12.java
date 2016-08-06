f76cf2be7b2b5e7aeb262905bea48ae5ada6c81b
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-08-06 17:11:12 +0200

    Exercise Ex 1.1.12

---
+++ src/ch_1_1/Ex_1_1_12.java
@@ -0,0 +1,25 @@
+package ch_1_1;
+
+/**
+ * 1.1.12 What does the following code fragment print?
+ * int[] a = new int[10];
+ * for (int i = 0; i < 10; i++)
+ * a[i] = 9 - i;
+ * for (int i = 0; i < 10; i++)
+ * a[i] = a[a[i]];
+ * for (int i = 0; i < 10; i++)
+ * System.out.println(i);
+ */
+public class Ex_1_1_12 {
+
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++)
+            a[i] = 9 - i;
+        for (int i = 0; i < 10; i++)
+            a[i] = a[a[i]];
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
+    }
+
+}
