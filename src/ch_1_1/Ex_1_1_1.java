3288572e3bb5a04b31ced5ed3311d517c2def6c2
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-08-06 17:09:28 +0200

    Exercise Ex 1.1.1

---
+++ src/ch_1_1/Ex_1_1_1.java
@@ -0,0 +1,16 @@
+package ch_1_1;
+
+/**
+ * 1.1.1 Give the value of each of the following expressions:
+ * a. ( 0 + 15 ) / 2
+ * b. 2.0e-6 * 100000000.1
+ * c.
+ * true && false || true && true
+ */
+public class Ex_1_1_1 {
+    public static void main(String[] args) {
+        System.out.println("( 0 + 15 ) / 2 = " + ((0 + 15) / 2));
+        System.out.println("2.0e-6 * 100000000.1 = " + (2.0e-6 * 100000000.1));
+        System.out.println("true && false || true && true = " + (true && false || true && true));
+    }
+}
