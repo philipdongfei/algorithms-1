b9d0a89243edf954ad287c402fe5ae884076d651
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-08-06 17:13:20 +0200

    Exercise Ex 1.1.25

---
+++ src/ch_1_1/Ex_1_1_25.java
@@ -0,0 +1,10 @@
+package ch_1_1;
+
+/**
+ * 1.1.25 Use mathematical induction to prove that Euclid’s algorithm computes the
+ * greatest common divisor of any pair of nonnegative integers p and q.
+ * <p>
+ * Read: https://math.stackexchange.com/questions/1274515/prove-that-euclids-algorithm-computes-the-gcd-of-any-pair-of-nonnegative-intege
+ */
+public class Ex_1_1_25 {
+}
