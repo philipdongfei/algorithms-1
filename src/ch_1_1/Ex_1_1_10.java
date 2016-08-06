8cceda7d7fab64631073413a0b9ad93c57f162d3
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-08-06 17:11:00 +0200

    Exercise Ex 1.1.10

---
+++ src/ch_1_1/Ex_1_1_10.java
@@ -0,0 +1,21 @@
+package ch_1_1;
+
+/**
+ * 1.1.10 What is wrong with the following code fragment?
+ * int[] a;
+ * for (int i = 0; i < 10; i++)
+ * a[i] = i * i;
+ * Solution: It does not allocate memory for a[] with new . This code results in a
+ * variable a might not have been initialized compile-time error.
+ */
+public class Ex_1_1_10 {
+
+    public static void main(String[] args) {
+        int[] a;
+        // there is lack of allocation memory.
+        for (int i = 0; i < 10; i++) {
+//            a[i] = i * i;
+        }
+    }
+
+}
