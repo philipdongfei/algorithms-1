ca561d4f1e7c6dc8029f041cbe3c4dcf32f5dcee
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-08-06 17:13:12 +0200

    Exercise Ex 1.1.24

---
+++ src/ch_1_1/Ex_1_1_24.java
@@ -0,0 +1,12 @@
+package ch_1_1;
+
+/**
+ * 1.1.24 Give the sequence of values of p and q that are computed when Euclid’s algorithm
+ * is used to compute the greatest common divisor of 105 and 24. Extend the code
+ * given on page 4 to develop a program Euclid that takes two integers from the command
+ * line and computes their greatest common divisor, printing out the two arguments for
+ * each call on the recursive method. Use your program to compute the greatest common
+ * divisor or 1111111 and 1234567.
+ */
+public class Ex_1_1_24 {
+}
