8aa86a1f8a71e05246b0ea0320af5e49d84fc130
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-08-07 16:00:25 +0200

    Final solution for Ex 1.1.24

--- src/ch_1_1/Ex_1_1_24.java
+++ src/ch_1_1/Ex_1_1_24.java
@@ -1,5 +1,7 @@
 package ch_1_1;
 
+import edu.princeton.cs.algs4.In;
+
 /**
  * 1.1.24 Give the sequence of values of p and q that are computed when Euclid’s algorithm
  * is used to compute the greatest common divisor of 105 and 24. Extend the code
@@ -9,4 +11,20 @@ package ch_1_1;
  * divisor or 1111111 and 1234567.
  */
 public class Ex_1_1_24 {
+
+  public static void main(String[] args) {
+    int p = Integer.parseInt(args[0]);
+    int q = Integer.parseInt(args[1]);
+    System.out.println(gcd(p, q));
+  }
+
+  public static int gcd(int p, int q) {
+    System.out.println("p=" + p + " q=" + q);
+    if (q == 0) {
+      return p;
+    }
+    int r = p % q;
+    return gcd(q, r);
+  }
+
 }
