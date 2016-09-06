6800d594b55f55045ab0945a35ac7fc2ca18c9d7
Piotr Piotrowski <p.piotrowski@samsung.com>
2016-09-06 14:57:25 +0200

    ex 1.1.26

--- src/ch_1_1/Ex_1_1_26.java
+++ src/ch_1_1/Ex_1_1_26.java
@@ -10,6 +10,15 @@ if (b > c) { t = b; b = c; c = t; }
 */
 public class Ex_1_1_26 {
   public static void main(String[] args) {
+
+    int a = 0;
+    int b = 0;
+    int t = 0;
+    int c = 0;
+
+    if (a > b) { t = a; a = b; b = t; }
+    if (a > c) { t = a; a = c; c = t; }
+    if (b > c) { t = b; b = c; c = t; }
     
   }
 }
