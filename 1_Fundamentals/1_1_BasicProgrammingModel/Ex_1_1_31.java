4c391cc37b3dc9bc5eb41eda5959e25e4418f3a4
Piotr Piotrowski <p.piotrowski@samsung.com>
2016-11-29 15:07:20 +0100

    partial solution for Ex1.1.31

--- 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_31.java
+++ 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_31.java
@@ -1,5 +1,21 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 /**
- * Created by piotr on 20.09.16.
+ * 1.1.31 Random connections.
+ * Write a program that takes as command-line arguments
+ * an integer N
+ * and a double value p (between 0 and 1),
+ * plots N equally spaced dots of size .05 on the circumference of a circle,
+ * and then,
+ * with probability p for each pair of points, draws a gray line connecting them.
  */
 public class Ex_1_1_31 {
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        double p = Math.max(0, Math.min(1, StdIn.readDouble()));
+        StdOut.println(p);
+    }
+
 }
