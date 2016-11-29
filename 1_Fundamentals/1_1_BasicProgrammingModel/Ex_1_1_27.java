84db70b1aeea6bc906709bd72f762f83ae2ec4df
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-11-29 17:37:28 +0100

    solution for Ex_1_1_27

--- 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_27.java
+++ 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_27.java
@@ -14,7 +14,9 @@ import edu.princeton.cs.algs4.StdOut;
  */
 public class Ex_1_1_27 {
 
-    static int count = 0;
+    static long count = 0;
+
+    private static double binomial[][] = new double[101][52];
 
     public static void main(String[] args) {
         double result = binomial(100, 50, 0.51);
@@ -24,8 +26,23 @@ public class Ex_1_1_27 {
 
     private static double binomial(int N, int k, double p) {
         count++;
+        if (count % 1000000000 == 0)
+            StdOut.println(count + " " + N  + " " + k);
         if ((N == 0) || (k < 0)) return 1.0;
-        return (1.0 - p) * binomial(N-1, k, p) + p*binomial(N-1, k-1, p);
+        return (1.0 - p) * binomial(N-1, k, p) + p * binomial(N-1, k-1, p);
+    }
+
+    private static double betterBinomial(int N, int k, double p) {
+        count++;
+        StdOut.println(count);
+        if ((N == 0) || (k < 0)) return 1.0;
+        if (binomial[N-1][k+1] == 0.0) {
+            binomial[N-1][k+1] = betterBinomial(N-1, k, p);
+        }
+        if (binomial[N-1][k] == 0.0) {
+            binomial[N-1][k] = betterBinomial(N-1, k-1, p);
+        }
+        return (1.0 - p) * binomial[N-1][k+1] + p * binomial[N-1][k];
     }
 
 }
