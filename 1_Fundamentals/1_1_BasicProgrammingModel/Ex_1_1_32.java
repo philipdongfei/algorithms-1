f962aa40613357547bb5a313032a2a29dca32e3d
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-12-07 22:56:36 +0100

    beginning of Ex_1_1_32

--- 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_32.java
+++ 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_32.java
@@ -1,5 +1,13 @@
 /**
- * Created by piotr on 20.09.16.
+ * 1.1.32 Histogram.
+ * Suppose that the standard input stream is a sequence of double values.
+ * Write a program that takes an integer N and two double values l and r from the command line
+ * and uses StdDraw to plot a histogram of the count of the numbers in the standard input stream
+ * that fall in each of the N intervals defined by dividing (l , r) into N equal-sized intervals.
  */
 public class Ex_1_1_32 {
+
+    public static void main(String[] args) {
+    }
+
 }
