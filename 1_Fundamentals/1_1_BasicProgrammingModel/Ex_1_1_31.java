3e89b0f78f5090ef7de5083e8ddc13a4bbe04276
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-12-07 22:52:46 +0100

    solution for Ex_1_1_31

--- 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_31.java
+++ 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_31.java
@@ -1,5 +1,8 @@
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.awt.*;
 
 /**
  * 1.1.31 Random connections.
@@ -15,7 +18,36 @@ public class Ex_1_1_31 {
     public static void main(String[] args) {
         int N = StdIn.readInt();
         double p = Math.max(0, Math.min(1, StdIn.readDouble()));
-        StdOut.println(p);
+        plot(N, p);
+    }
+
+    private static void plot(int n, double p) {
+        StdDraw.setCanvasSize(500, 500);
+        StdDraw.setXscale(-1.1, 1.1);
+        StdDraw.setYscale(-1.1, 1.1);
+        StdDraw.setPenRadius(0.05);
+
+        // table with points
+        double[][] points = new double[n][2];
+
+        // calculates points
+        for (int i=0; i<n; i++) {
+            double angle = 2 * Math.PI * i / n;
+            points[i][0] = Math.sin(angle);
+            points[i][1] = Math.cos(angle);
+            StdDraw.point(points[i][0], points[i][1]);
+        }
+        StdDraw.setPenRadius(0.001);
+        StdDraw.setPenColor(Color.GRAY);
+
+        // randomly draw lines between points depending on given probability
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if (i!=j && StdRandom.bernoulli(p)) {
+                    StdDraw.line(points[i][0], points[i][1], points[j][0], points[j][1]);
+                }
+            }
+        }
     }
 
 }
