65c563c1853c7820c8f1bcd17a64bc32b1d51860
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-12-16 20:26:25 +0100

    beginning of Ex_1_1_33

--- 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_33.java
+++ 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_33.java
@@ -1,5 +1,13 @@
 /**
- * Created by piotr on 20.09.16.
+ * 1.1.33 Matrix library. Write a library Matrix that implements the following API:
+ * public class Matrix
+ *  static double dot(double[] x, double[] y) vector dot product
+ *  static double[][] mult(double[][] a, double[][] b) matrix-matrix product
+ *  static double[][] transpose(double[][] a) transpose
+ *  static double[] mult(double[][] a, double[] x) matrix-vector product
+ *  static double[] mult(double[] y, double[][] a) vector-matrix product
+ *
+ *  Develop a test client that reads values from standard input and tests all the methods.
  */
 public class Ex_1_1_33 {
 }
