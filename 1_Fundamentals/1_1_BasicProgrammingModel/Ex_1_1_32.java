303eb69fecfafe6c2b142202cd22e3676d6b04cc
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-12-16 20:25:59 +0100

    solution for Ex_1_1_32

--- 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_32.java
+++ 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_32.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdStats;
+
 /**
  * 1.1.32 Histogram.
  * Suppose that the standard input stream is a sequence of double values.
@@ -8,6 +12,42 @@
 public class Ex_1_1_32 {
 
     public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        double l = Double.parseDouble(args[1]);
+        double r = Double.parseDouble(args[2]);
+        System.out.println("N=" + N + " l=" + l + " r=" + r);
+
+        double[] values = StdIn.readAllDoubles();
+
+        drawHistogram(N, l, r, values);
+    }
+
+    private static void drawHistogram(int N, double l, double r, double[] values) {
+        int[] counts = new int[N+1];
+        for (double value: values) {
+            if (value <= r && value >= l) {
+                counts[getInteval(value, N, l, r)]++;
+            }
+        }
+        int maxCount = StdStats.max(counts);
+
+        double w = (r - l) / N;
+        double rw = 0.5 * w;
+
+        StdDraw.setCanvasSize(1024, 512);
+        StdDraw.setXscale(l, r+w);
+        StdDraw.setYscale(0, maxCount);
+
+        for (int i = 0; i <= N; i++) {
+            double x = l + (i + 0.5) * w,
+                    y = counts[i] / 2.0,
+                    rh = counts[i] / 2.0;
+            StdDraw.filledRectangle(x, y, rw, rh);
+        }
+    }
+
+    private static int getInteval(double value, int N, double l, double r) {
+        return (int) (N*(value - l)/(r-l));
     }
 
 }
