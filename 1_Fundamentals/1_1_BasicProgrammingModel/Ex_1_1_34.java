b6f382488137b5f0d6f440cf2dd6fb1d8fef3d77
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2017-01-17 22:16:48 +0100

    Solution for Ex_1_1_34

--- 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_34.java
+++ 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_34.java
@@ -1,5 +1,21 @@
 /**
- * Created by piotr on 20.09.16.
+ * 1.1.34 Filtering. Which of the following require saving all the values from standard
+ * input (in an array, say), and which could be implemented as a filter using only a fixed
+ * number of variables and arrays of fixed size (not dependent on N)? For each, the input
+ * comes from standard input and consists of N real numbers between 0 and 1.
+ *  - Print the maximum and minimum numbers.
+ *  - Print the median of the numbers.
+ *  - Print the k th smallest value, for k less than 100.
+ *  - Print the sum of the squares of the numbers.
+ *  - Print the average of the N numbers.
+ *  - Print the percentage of numbers greater than the average.
+ *  - Print the N numbers in increasing order.
+ *  - Print the N numbers in random order.
+ *
+ *  @author <a href="piotr@piotrovski.com">Piotr Piotrowski</a>
  */
 public class Ex_1_1_34 {
+
+    // solution for this excercise is by answer not by code.
+
 }
