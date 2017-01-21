ed1b7aa73e00aaf2e2e03cb95c363b8722138ba7
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2017-01-21 15:07:46 +0100

    description of Ex_1_1_36

--- 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_36.java
+++ 1_Fundamentals/1_1_BasicProgrammingModel/Ex_1_1_36.java
@@ -1,5 +1,17 @@
 /**
- * Created by piotr on 20.09.16.
+ * 1.1.36 Empirical shuffle check.
+ * Run computational experiments to check that our shuffling code on page 32 works as advertised.
+ * Write a program ShuffleTest that takes command-line arguments M and N,
+ * does N shuffles of an array of size M that is initialized with a[i] = i before each shuffle,
+ * and prints an M-by-M table such that row i gives the number of times i wound up in position j for all j.
+ * All entries in the array should be close to N/M.
+ *
+ * @author <a href="mailto:piotr@piotrovski.com">Piotr Piotrowski</a>
  */
 public class Ex_1_1_36 {
+
+    public static void main(String[] args) {
+
+    }
+
 }
