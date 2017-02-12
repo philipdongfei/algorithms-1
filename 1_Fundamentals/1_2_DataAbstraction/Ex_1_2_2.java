226e7c64217554f933736ca5e786c0f51f8f347e
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2017-02-12 21:30:50 +0100

    Solution for Ex_1_2_2

--- 1_Fundamentals/1_2_DataAbstraction/Ex_1_2_2.java
+++ 1_Fundamentals/1_2_DataAbstraction/Ex_1_2_2.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.Interval1D;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * 1.2.2 Write an Interval1D client that takes an int value N as command-line argument,
  * reads N intervals (each defined by a pair of double values) from standard input,
@@ -8,6 +12,20 @@
 public class Ex_1_2_2 {
 
     public static void main(String[] args) {
+        int N = StdIn.readInt();
+        Interval1D[] intervals = new Interval1D[N];
+
+        for (int i=0; i<N; i++) {
+            intervals[i] = new Interval1D(StdIn.readDouble(), StdIn.readDouble());
+        }
+
+        for (int i=0; i<N; i++) {
+            for (int j=i+1; j<N; j++) {
+                if (intervals[i].intersects(intervals[j])) {
+                    StdOut.println(intervals[i] + "-" + intervals[j]);
+                }
+            }
+        }
     }
 
 }
