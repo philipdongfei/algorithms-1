5eff54222b0da930d1cfcc6abb9273d1c6d903d8
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2017-02-16 21:56:14 +0100

    Solution for Ex_1_2_7

--- 1_Fundamentals/1_2_DataAbstraction/Ex_1_2_7.java
+++ 1_Fundamentals/1_2_DataAbstraction/Ex_1_2_7.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * 1.2.7 What does the following recursive function return?
  *
@@ -14,6 +16,16 @@
 public class Ex_1_2_7 {
 
     public static void main(String[] args) {
+        // function mystery returns as expected - reverse string
+        StdOut.println(mystery("mystery"));
+    }
+
+    public static String mystery(String s) {
+        int N = s.length();
+        if (N <= 1) return s;
+        String a = s.substring(0, N/2);
+        String b = s.substring(N/2, N);
+        return mystery(b) + mystery(a);
     }
 
 }
