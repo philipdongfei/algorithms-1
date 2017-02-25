e90bbc15422812538bc89ff6d596142ef6bd35b2
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2017-02-25 21:31:52 +0100

    Solution for Ex_1_3_4

--- 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_4.java
+++ 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_4.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * 1.3.4
  * Write a stack client Parentheses that reads in a text stream from standard input
@@ -9,7 +11,48 @@
 public class Ex_1_3_4 {
 
     public static void main(String[] args) {
+        if (!Parentheses.test("[()]{}{[()()]()}")) {
+            StdOut.println("string: [()]{}{[()()]()} is balanced");
+        }
+        if (Parentheses.test("[(])")) {
+            StdOut.println("string: [(]) is not balanced");
+        }
+    }
 
+}
+
+class Parentheses {
+
+    static boolean test(String stringToTest) {
+        FixedCapacityStackOfStrings s = new FixedCapacityStackOfStrings(stringToTest.length());
+        String currentCharacter;
+        String previousCharacter = "";
+        for (int i = 0; i < stringToTest.length(); i++) {
+            currentCharacter = "" + stringToTest.charAt(i);
+            if (!"".equals(previousCharacter) && reverse(previousCharacter).equals(currentCharacter)) {
+                s.pop();
+                if (!s.isEmpty()) {
+                    previousCharacter = s.pop();
+                    s.push(previousCharacter);
+                }
+                continue;
+            }
+            s.push(currentCharacter);
+            previousCharacter = currentCharacter;
+        }
+        return s.isEmpty();
+    }
+
+    private static String reverse(String str) {
+        if ("(".equals(str)) {
+            return ")";
+        } else if ("[".equals(str)) {
+            return "]";
+        } else if ("{".equals(str)) {
+            return "}";
+        } else {
+            return "";
+        }
     }
 
 }
