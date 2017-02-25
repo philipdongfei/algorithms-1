47f81a64baf319ba2ce3e1e08f30b7206b413d74
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2017-02-25 19:50:06 +0100

    Solution for Ex_1_3_2

--- 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_2.java
+++ 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_2.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * 1.3.2
  * Give the output printed by java Stack for the input
@@ -7,5 +9,15 @@
  */
 public class Ex_1_3_2 {
     public static void main(String[] args) {
+        FixedCapacityStackOfStrings s = new FixedCapacityStackOfStrings(100);
+        String input = "it was - the best - of times - - - it was - the - -";
+        for (String item :  input.split(" ")) {
+            if (!"-".equals(item)) {
+                s.push(item);
+            } else if (!s.isEmpty()) {
+                StdOut.print(s.pop() + " ");
+            }
+        }
+        StdOut.println("(" + s.size() + " left on stack)");
     }
 }
