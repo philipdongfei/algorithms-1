5a6b30f019417908a4712775f05c13d58146cf5e
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2017-02-26 21:14:52 +0100

    Solution for Ex_1_3_6

--- 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_6.java
+++ 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_6.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * 1.3.6
  * What does the following code fragment do to the queue q?
@@ -13,6 +17,18 @@
 public class Ex_1_3_6 {
 
     public static void main(String[] args) {
+        Queue<String> q = new Queue<>();
+        q.enqueue("first");
+        q.enqueue("second");
+
+        Stack<String> stack = new Stack<>();
+        while (!q.isEmpty())
+            stack.push(q.dequeue());
+        while (!stack.isEmpty())
+            q.enqueue(stack.pop());
+        StdOut.println();
+
+        // this piece of code reverse the elements on q.
     }
 
 }
