ae7df66fa8ae2810bf106850a77a69f162422eef
Piotr Piotrowski <p.piotrowski@samsung.com>
2017-03-01 14:04:08 +0100

    solution for Ex1.3.11

--- 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_11.java
+++ 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_11.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+
 /**
  * 1.3.11 Write a program EvaluatePostfix that takes a postfix expression from standard input, evaluates it,
  * and prints the value.
@@ -8,6 +11,29 @@
 public class Ex_1_3_11 {
 
     public static void main(String[] args) {
+        Stack<Double> vals = new Stack<>();
+        while(!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if ("+".equals(item)) {
+                // add two items
+                Double val1 = vals.pop();
+                vals.push(val1 + vals.pop());
+            } else if ("-".equals(item)) {
+                // subtract two items
+                Double val1 = vals.pop();
+                vals.push(vals.pop() - val1);
+            } else if ("*".equals(item)) {
+                // multiply two items
+                Double val1 = vals.pop();
+                vals.push(val1 * vals.pop());
+            } else if ("/".equals(item)) {
+                // divide two items
+                Double val1 = vals.pop();
+                vals.push(vals.pop() / val1);
+            } else {
+                vals.push(Double.parseDouble(item));
+            }
+        }
     }
 
 }
