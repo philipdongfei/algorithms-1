1a4e942dd211a6e88d98ba7b1ff71b5e345ca8fd
Piotr Piotrowski <p.piotrowski@samsung.com>
2017-03-01 14:03:08 +0100

    solution for Ex1.3.9

--- 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_9.java
+++ 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_9.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * 1.3.9
  * Write a program that takes from standard input an expression without left parentheses
@@ -5,13 +9,30 @@
  *
  *  1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )
  * your program should print
- *  ( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) )
+ *  ( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )
  *
  * @author <a href="mailto:piotr@piotrovski.com">Piotr Piotrowski</a>
  */
 public class Ex_1_3_9 {
 
     public static void main(String[] args) {
+        Stack<String> expression = new Stack<>();
+
+        while(!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+
+            if (item.equals(")")) {
+                String ex1 = expression.pop();
+                String op = expression.pop();
+                String ex2 = expression.pop();
+                expression.push("( " + ex2 + " " + op + " " + ex1 + " )");
+            } else {
+                expression.push(item);
+            }
+        }
+        for (String ex : expression) {
+            StdOut.print(ex);
+        }
     }
 
 }
