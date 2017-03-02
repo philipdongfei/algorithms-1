35abf5a642056a59c4a4563c0a44cbde160357b8
Piotr Piotrowski <p.piotrowski@samsung.com>
2017-03-02 09:43:14 +0100

    solution for Ex1.3.12

--- 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_12.java
+++ 1_Fundamentals/1_3_Bags_Queues_and_Stacks/Ex_1_3_12.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * 1.3.12
  * Write an iterable Stack client that has a static method copy() that takes a stack of strings as argument
@@ -10,6 +13,29 @@
 public class Ex_1_3_12 {
 
     public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("first");
+        stack.push("second");
+        Stack<String> copyOfStack = copy(stack);
+        for (String item:stack) {
+            boolean found = false;
+            for (String copyOfStackItem: copyOfStack) {
+                if (item.equals(copyOfStackItem)) {
+                    found = true;
+                }
+            }
+            if (!found) {
+                StdOut.println("something went wrong while copying stack. copy does not contain element" + item);
+            }
+        }
+    }
+
+    static Stack<String> copy(Stack<String> stack) {
+        Stack<String> result = new Stack<>();
+        for (String item: stack) {
+            result.push(item);
+        }
+        return result;
     }
 
 }
